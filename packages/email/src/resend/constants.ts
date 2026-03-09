0836d3dfca791f13bb3944feef35c6caad28f212
Karan Singh <official.jaskaran13@gmail.com>
2026-03-09 21:16:39 +0530

    fix email env bug

--- packages/email/src/resend/constants.ts
+++ packages/email/src/resend/constants.ts
@@ -1,7 +1,7 @@
 export const RESEND_AUDIENCE_ID = "f5ff0661-4234-43f6-b0ca-a3f3682934e3";
 
 export const VARIANT_TO_FROM_MAP = {
-  primary: "BoilerCode <noreply@sendboilercode.dev>",
+  primary: "BoilerCode <noreply@send.boilercode.dev>",
   notifications: "BoilerCode <notifications@sendboilercode.dev>",
   marketing: "Karan from BoilerCode <karan@sendboilercode.dev>",
 };
