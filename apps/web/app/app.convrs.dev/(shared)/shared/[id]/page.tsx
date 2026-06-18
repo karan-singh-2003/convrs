d14746c904c00f9f5d65cff128054810e1f71ee2
unknown <official.jaskaran13@gmail.com>
2026-06-18 07:39:24 +0530

    fix:publicdashboard

--- apps/web/app/app.convrs.dev/(shared)/shared/[id]/page.tsx
+++ apps/web/app/app.convrs.dev/(shared)/shared/[id]/page.tsx
@@ -41,11 +41,11 @@ export default async function SharedAnalyticsPage({ params }: PageProps) {
         <nav className="mx-auto flex w-full max-w-screen-lg items-center justify-between gap-4 px-4 md:px-0">
           <div className="flex items-center gap-2.5 font-display text-sm font-medium text-neutral-600">
             <h1 className="font-semibold font-poppins px-1 text-[14.5px]">
-              Convrs
+                {workspace.name}
             </h1>
-            <h1 className="text-[13px] font-medium text-neutral-500 bg-neutral-100 px-2 py-1 rounded-full">
+            {/* <h1 className="text-[13px] font-medium text-neutral-500 bg-neutral-100 px-2 py-1 rounded-full">
               {workspace.name}{" "}
-            </h1>
+            </h1> */}
           </div>
           <Button
             text="Get Started"
